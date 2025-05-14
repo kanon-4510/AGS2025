543fa3ea3efb5ec9ccae6e5d7891f8d3eee39ea6
拓海 中山 <enamel.0909@gmail.com>
2025-05-14 12:35:11 +0900

    push

--- Src/Scene/TitleScene.cpp
+++ Src/Scene/TitleScene.cpp
@@ -59,7 +59,7 @@ void TitleScene::Init(void)
 
 	// キャラ
 	charactor_.SetModel(resMng_.LoadModelDuplicate(ResourceManager::SRC::PLAYER));
-	charactor_.pos = { 0.0f,-100.0f,0.0f };
+	charactor_.pos = { 0.0f,-200.0f,0.0f };
 	size = 1.4f;
 	charactor_.scl = { size, size, size };
 	charactor_.quaRot = Quaternion::Euler(
@@ -69,7 +69,7 @@ void TitleScene::Init(void)
 	// アニメーションの設定
 	std::string path = Application::PATH_MODEL + "Player/";
 	animationController_ = std::make_unique<AnimationController>(charactor_.modelId);
-	animationController_->Add(0, path + "Slash.mv1", 20.0f);
+	animationController_->Add(0, path + "Sword And Shield Death.mv1", 20.0f);
 	animationController_->Play(0);
 
 	// 定点カメラ
@@ -103,17 +103,13 @@ void TitleScene::Draw(void)
 {
 	//skyDome_->Draw();
 	
-	
-
 	DrawRotaGraph(Application::SCREEN_SIZE_X / 2, Application::SCREEN_SIZE_Y / 2, 1.0, 0.0, imgBackTitle_, true);
 
 	DrawRotaGraph(Application::SCREEN_SIZE_X / 2, 500, 1.0, 0.0, imgTitle_, true);
 	
 	DrawRotaGraph(Application::SCREEN_SIZE_X / 2, 500, 1.0, 0.0, imgPush_, true);
 
-	
-
 	//MV1DrawModel(planet_.modelId);
 	//MV1DrawModel(movePlanet_.modelId);
-	//MV1DrawModel(charactor_.modelId);
+	MV1DrawModel(charactor_.modelId);
 }
