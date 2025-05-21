8162bb7ba498dd2e2ce372865c4462ede36f97f5
拓海 中山 <enamel.0909@gmail.com>
2025-05-21 12:45:50 +0900

    木

--- Src/Object/Tree.cpp
+++ Src/Object/Tree.cpp
@@ -25,14 +25,14 @@ Tree::~Tree(void)
 
 bool Tree::Init(GameScene* parent)
 {
-	modelIdB_ = MV1LoadModel((Application::PATH_MODEL + "wood/1.mv1").c_str());
-	modelIdK_ = MV1LoadModel((Application::PATH_MODEL + "wood/1.mv1").c_str());
-	modelIdA_ = MV1LoadModel((Application::PATH_MODEL + "wood/1.mv1").c_str());
-	modelIdO_ = MV1LoadModel((Application::PATH_MODEL + "wood/1.mv1").c_str());
+	modelIdB_ = MV1LoadModel((Application::PATH_MODEL + "wood/Baby.mv1").c_str());
+	modelIdK_ = MV1LoadModel((Application::PATH_MODEL + "wood/Kid.mv1").c_str());
+	modelIdA_ = MV1LoadModel((Application::PATH_MODEL + "wood/Adult.mv1").c_str());
+	modelIdO_ = MV1LoadModel((Application::PATH_MODEL + "wood/Old.mv1").c_str());
 
-	scl_ = { 1.0f, 1.0f, 1.0f };						// 大きさの設定
+	scl_ = { 2.0f, 2.0f, 2.0f };						// 大きさの設定
 	rot_ = { 0.0f, 0.0f * DX_PI_F / 180.0f, 0.0f };		// 角度の設定
-	pos_ = { 00.0f, -10.0f, 0.0f };					// 位置の設定
+	pos_ = { 0.0f, 60.0f, 0.0f };						// 位置の設定
 	dir_ = { 0.0f, 90.0f, 0.0f };						// 右方向に移動する
 
 	lv_ = 1;
@@ -136,29 +136,22 @@ void Tree::ChangeGrow(void)
 	if (lv_ == 75)
 	{
 		grow_ = Tree::GROW::OLD;
-		scl_ = { 5.0f, 5.0f, 5.0f };
-		pos_ = { 00.0f, -10.0f, 0.0f };
+		scl_ = { 50.0f, 45.0f, 50.0f };
+		pos_ = { 00.0f, 1470.0f, 0.0f };
 		hp_ = 50;
 	}
 	else if (lv_ == 50)
 	{
 		grow_ = Tree::GROW::ADULT;
-		scl_ = {3.0f, 3.0f, 3.0f };
-		pos_ = { 00.0f, -10.0f, 0.0f };
+		scl_ = { 30.0f, 25.0f, 30.0f };
+		pos_ = { 0.0f, 805.0f, 0.0f };
 		hp_ = 50;
 	}
 	else if (lv_ == 25)
 	{
 		grow_ = Tree::GROW::KID;
-		scl_ = { 2.0f, 2.0f, 2.0f };
-		pos_ = { 00.0f, -10.0f, 0.0f };
-		hp_ = 50;
-	}
-	else if (lv_ == 1)
-	{
-		grow_ = Tree::GROW::BABY;
-		scl_ = { 1.0f, 1.0f, 1.0f };
-		pos_ = { 00.0f, -10.0f, 0.0f };
+		scl_ = { 15.0f, 10.0f, 15.0f };
+		pos_ = { 00.0f, 450.0f, 0.0f };
 		hp_ = 50;
 	}
 }
