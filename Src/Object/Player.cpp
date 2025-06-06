a237d80e540c499c753b19785eacbc645cdf1626
2316019 <reotakano2@gmail.com>
2025-06-06 14:01:08 +0900

    player

--- Src/Object/Player.cpp
+++ Src/Object/Player.cpp
@@ -76,7 +76,7 @@ void Player::Init(void)
 	transform_.SetModel(resMng_.LoadModelDuplicate(
 		ResourceManager::SRC::PLAYER));
 	transform_.scl = AsoUtility::VECTOR_ONE;
-	transform_.pos = { 0.0f, -30.0f, 0.0f };
+	transform_.pos = { 300.0f, -30.0f, 0.0f };
 	transform_.quaRot = Quaternion();
 	transform_.quaRotLocal =
 		Quaternion::Euler({ 0.0f, AsoUtility::Deg2RadF(180.0f), 0.0f });
