e58e32cf50099a3c97832bb7f205f9c05ce13937
kanon-4510 <mobuyuki.png@gmail.com>
2026-01-23 09:52:03 +0900

    おｋ

--- Src/Manager/InputManager.cpp
+++ Src/Manager/InputManager.cpp
@@ -298,6 +298,38 @@ InputManager::JOYPAD_IN_STATE InputManager::GetJPadInputState(JOYPAD_NO no)
 		idx = static_cast<int>(JOYPAD_BTN::DOWN);
 		ret.ButtonsNew[idx] = d.Buttons[0];// A
 
+		//十字キー
+		idx = static_cast<int>(JOYPAD_BTN::DG_UP);
+		ret.ButtonsNew[idx] = x.Buttons[0];// 上
+		
+		idx = static_cast<int>(JOYPAD_BTN::DG_DOWN);
+		ret.ButtonsNew[idx] = x.Buttons[1];// 下
+		
+		idx = static_cast<int>(JOYPAD_BTN::DG_LEFT);
+		ret.ButtonsNew[idx] = x.Buttons[2];// 左
+		
+		idx = static_cast<int>(JOYPAD_BTN::DG_RIGHT);
+		ret.ButtonsNew[idx] = x.Buttons[3];// 右
+
+		idx = static_cast<int>(JOYPAD_BTN::STRAT);
+		ret.ButtonsNew[idx] = x.Buttons[4];// メニュー
+
+		idx = static_cast<int>(JOYPAD_BTN::BACK);
+		ret.ButtonsNew[idx] = x.Buttons[5];// バック
+
+		idx = static_cast<int>(JOYPAD_BTN::L3);
+		ret.ButtonsNew[idx] = x.Buttons[6];// Lスティック押込み
+
+		idx = static_cast<int>(JOYPAD_BTN::R3);
+		ret.ButtonsNew[idx] = x.Buttons[7];// Rスティック押込み
+
+		//トリガー
+		idx = static_cast<int>(JOYPAD_BTN::LB);
+		ret.ButtonsNew[idx] = x.Buttons[8];// LB
+
+		idx = static_cast<int>(JOYPAD_BTN::RB);
+		ret.ButtonsNew[idx] = x.Buttons[9];// RB
+
 		idx = static_cast<int>(JOYPAD_BTN::R_TRIGGER);
 		ret.ButtonsNew[idx] = x.RightTrigger;// R_TRIGGER
 
@@ -375,6 +407,34 @@ bool InputManager::IsPadBtnTrgDown(JOYPAD_NO no, JOYPAD_BTN btn) const
 bool InputManager::IsPadBtnTrgUp(JOYPAD_NO no, JOYPAD_BTN btn) const
 {
 	return padInfos_[static_cast<int>(no)].IsTrgUp[static_cast<int>(btn)];
+}
+
+VECTOR InputManager::GetDirectionXZAKey(int aKeyX, int aKeyY)
+{
+	VECTOR ret = { 0.0f, 0.0f, 0.0f };
+	// スティックの個々の入力値は、
+	// -1000.0f ～ 1000.0f の範囲で返ってくるが、
+	// X:1000.0f、Y:1000.0fになることは無い(1000と500くらいが最大)
+	// スティックの入力値を -1.0 ～ 1.0 に正規化
+	float dirX = static_cast<float>(aKeyX) / AKEY_VAL_MAX;
+	float dirZ = static_cast<float>(aKeyY) / AKEY_VAL_MAX;
+	// ピタゴラスの定理でニュートラル状態からの長さベクトルにする
+	// ( 円形のデッドゾーンになる )
+	// 平方根により、おおよその最大値が1.0となる
+	float len = sqrtf(dirX * dirX + dirZ * dirZ);
+	if (len < THRESHOLD)
+	{
+		// (0.0f, 0.0f, 0.0f)
+		return ret;
+	}
+	// デッドゾーン境界からに再スケーリング(可変デッドゾーン)
+	// ( しきい値 0.35 の場合は、 0.0 ～ 0.65 / 0.65 になる )
+	float scale = (len - THRESHOLD) / (1.0f - THRESHOLD);
+	dirX = (dirX / len) * scale;
+	dirZ = (dirZ / len) * scale;
+	// Zは前に倒すとマイナス値が返ってくるので反転
+	ret = VNorm({ dirX, 0.0f, -dirZ });
+	return ret;
 }
 
 
